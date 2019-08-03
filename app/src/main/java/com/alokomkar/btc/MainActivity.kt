ca9a2f60d11c11b5c46a7ea8b9c2920d2e5e4b7b
Alok <alok@wizely.in>
2019-08-03 11:19:29 +0530

    Corrected repository implementaion

--- app/src/main/java/com/alokomkar/btc/MainActivity.kt
+++ app/src/main/java/com/alokomkar/btc/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.alokomkar.btc
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
@@ -31,86 +27,73 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun showToast(message: String) {
-
-    }
+    private fun showToast(message: String) =
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
     private fun loadContent() {
-        observe(priceViewModel.getCurrentPrice()) {
+        priceViewModel.getCurrentPrice()
+        observe(priceViewModel.currentPriceLiveData) {
             it?.apply {
-                pbCurrentPrice.changeVisibility(status == Status.LOADING )
-                tvCurrentPrice.changeVisibility( status != Status.LOADING )
-                if( status == Status.ERROR ) {
-                    Log.d("NetworkResponse", "Failed : " + this.message)
+                pbCurrentPrice.changeVisibility(status == Status.LOADING)
+                tvCurrentPrice.changeVisibility(status != Status.LOADING)
+                if (status == Status.ERROR) {
                     tvCurrentPrice.showSnackBar(this.message ?: "Unknown Error", R.string.retry) {
                         loadContent()
                     }
-                    tvCurrentPrice.text = "Oops!!"
+                    tvCurrentPrice.text = getString(R.string.oops)
+                } else if (status == Status.SUCCESS) {
+                    tvCurrentPrice.text = "Current Price : $ ${this.data?.sellingPrice}"
+                }
+            }
+        }
+
+        priceViewModel.getPriceHistory(false)
+        observe(priceViewModel.priceHistoryLiveData) {
+            it?.apply {
+                pbPriceHistory.changeVisibility(status == Status.LOADING)
+                priceRefreshLayout.isRefreshing = (status == Status.LOADING )
+                if( status == Status.ERROR ) {
+                    showToast((this.message ?: "Unknown Error") + " : checking offline data")
+                    priceViewModel.getPriceHistory(true)
                 }
                 else if( status == Status.SUCCESS ) {
-                    Log.d("NetworkResponse", "Success " + this.data?.sellingPrice)
-                    tvCurrentPrice.text = "Current Price : $ " + this.data?.sellingPrice.toString()
-                }
-            }
-        }
-        fun fetchPriceHistory( fetchFromLocal : Boolean ) {
-            observe(priceViewModel.getPriceHistory(fetchFromLocal)) {
-                it?.apply {
-                    pbPriceHistory.changeVisibility(status == Status.LOADING)
-                    priceRefreshLayout.isRefreshing = (status == Status.LOADING )
-                    if( status == Status.ERROR ) {
-                        Log.d("NetworkResponse", "Failed : " + this.message)
-                        showToast((this.message ?: "Unknown Error") + " : checking offline data")
-                        fetchPriceHistory(true)
-                    }
-                    else if( status == Status.SUCCESS ) {
-                        Log.d("NetworkResponse", "Price history Success " + this.data?.size)
-                        rvPriceHistory.apply {
-                            adapter = PriceHistoryAdapter().apply {
-                                removeAll()
-                                val items = data ?: ArrayList()
-                                var previousHeader = ""
-                                items.forEach{ item ->
-                                    if( previousHeader == "" ) {
-                                        item.header = item.getPriceDate()
-                                        previousHeader = item.header
-                                    }
-                                    else if( previousHeader != item.getPriceDate() ) {
-                                        item.header = item.getPriceDate()
-                                        previousHeader = item.header
-                                    }
+                    rvPriceHistory.apply {
+                        adapter = PriceHistoryAdapter().apply {
+                            removeAll()
+                            val items = data ?: ArrayList()
+                            var previousHeader = ""
+                            items.forEach{ item ->
+                                if( previousHeader == "" ) {
+                                    item.header = item.getPriceDate()
+                                    previousHeader = item.header
                                 }
-                                addAll(items)
+                                else if( previousHeader != item.getPriceDate() ) {
+                                    item.header = item.getPriceDate()
+                                    previousHeader = item.header
+                                }
                             }
+                            addAll(items)
                         }
-                        rvDate.apply {
-                            adapter = PriceDateAdapter().apply {
-                                removeAll()
-                                var itemIndex = 0
-                                data?.forEach {
-                                        item ->
-                                    addUnique(PriceIndexedData(itemIndex, item.getPriceDate()))
-                                    itemIndex++
-                                }
-                                onItemClickListener = { _, _, item ->
-                                    Log.d("SmoothScroll", "Position : " + item.index)
-                                    rvPriceHistory.smoothScrollToPosition(item.index + 5)
-                                }
+                    }
+                    rvDate.apply {
+                        adapter = PriceDateAdapter().apply {
+                            removeAll()
+                            var itemIndex = 0
+                            data?.forEach {
+                                    item ->
+                                addUnique(PriceIndexedData(itemIndex, item.getPriceDate()))
+                                itemIndex++
+                            }
+                            onItemClickListener = { _, _, item ->
+                                rvPriceHistory.smoothScrollToPosition(item.index + 5)
                             }
                         }
                     }
                 }
             }
         }
-        fetchPriceHistory(false)
 
     }
 
-    class NetworkConnectivityReceiver : BroadcastReceiver() {
-        override fun onReceive(contxt: Context?, intent: Intent?) {
-
-        }
-
-    }
 
 }
--- app/src/main/java/com/alokomkar/btc/PriceViewModel.kt
+++ app/src/main/java/com/alokomkar/btc/PriceViewModel.kt
@@ -1,14 +1,28 @@
 package com.alokomkar.btc
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.alokomkar.btc.base.BaseViewModel
+import com.alokomkar.btc.data.Resource
+import com.alokomkar.btc.data.local.entity.CurrentPrice
+import com.alokomkar.btc.data.local.entity.PriceHistory
 import com.alokomkar.btc.data.repository.BTCRepository
 
 class PriceViewModel( application: Application ) : BaseViewModel(application) {
 
     private val repository : BTCRepository = btcApplication.btcRepository
 
-    fun getCurrentPrice() = repository.getCurrentPrice()
-    fun getPriceHistory( fetchOffline : Boolean ) = repository.getPriceHistory(fetchOffline)
+    val currentPriceLiveData : LiveData<Resource<CurrentPrice>> = MutableLiveData()
+    val priceHistoryLiveData : LiveData<Resource<List<PriceHistory>>> = MutableLiveData()
+
+    fun getCurrentPrice() {
+        currentPriceLiveData as MutableLiveData
+        currentPriceLiveData.value = repository.getCurrentPrice().value
+    }
+    fun getPriceHistory( fetchOffline : Boolean ) {
+        priceHistoryLiveData as MutableLiveData
+        priceHistoryLiveData.value = repository.getPriceHistory(fetchOffline).value
+    }
 
 }
